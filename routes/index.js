6a22bfb01d51e320fe5f700143c957e6e333e822
Estotive <martinyuk.d.i.20@gmail.com>
2022-06-25 23:32:20 +0300

    4.2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* Страница Нацу */
+router.get('/natsu', function(req, res, next) {
+  res.send("<h1>Страница Нацу</h1>")
+});
+/* Страница "Эльзы" */
+router.get('/elza', function(req, res, next) {
+  res.send("<h1>Страница Эльзы</h1>")
+});
+/* Страница Хэппи */
+router.get('/happy', function(req, res, next) {
+  res.send("<h1>Страница Хэппи</h1>")
+});
+
+
 module.exports = router;
