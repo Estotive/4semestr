eb3e55e6485b9d078d43394b1794af968883b705
Estotive <martinyuk.d.i.20@gmail.com>
2022-06-26 23:24:39 +0300

    6.1-6.3

---
+++ data.js
@@ -0,0 +1,21 @@
+var data = [{
+    title: 'Нацу',
+    nick: 'natsu',
+    avatar: 'https://i.pinimg.com/736x/56/10/02/5610024548859fb781dd2e46a2d8a577.jpg',
+    desc: 'Огненный убийца драконов'
+},
+{
+    title: 'Эльза',
+    nick: 'elza',
+    avatar: "https://i.pinimg.com/564x/d3/b3/8d/d3b38d4683c8327e2f0ebf61f067a6b1.jpg",
+    desc: "Владеет магией 'Перевооружение: Рыцарь'"
+},
+{
+    title: 'Хэппи',
+    nick: 'happy',
+    avatar: "https://i.pinimg.com/564x/cf/1f/bc/cf1fbcdb1bcdad29c45dfd1056aa1e44.jpg",
+    desc: "Синий летающий кот"
+},
+];
+
+module.exports.data = data;
