63dc31241d9d032296b2a3d41c3fe7689bcb7e64
Estotive <martinyuk.d.i.20@gmail.com>
2022-06-27 01:21:35 +0300

    8.6

--- createDB.js
+++ createDB.js
@@ -1,87 +1,37 @@
-var async = require("async")
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
 var Hero = require("./models/hero").Hero
+var async = require("async")
+var data = require('./data.js').data
 
-mongoose.connection.on("open", function() {
-    var db = mongoose.connection.db
-    db.dropDatabase(function(err) {
-    async.series([
+async.series([
         open,
         dropDatabase,
         createHeroes,
         close
     ],
-    function(err, result) {
-        if (err) throw err
-
-        async.parallel([
-                function(callback) {
-                    var natsu = new Hero({ nick: 'natsu' })
-                    natsu.save(function(err, natsu) {
-                        callback(err, "natsu")
-                    })
-                },
-                function(callback) {
-                    var elza = new Hero({ nick: 'elza' })
-                    elza.save(function(err, elza) {
-                        callback(err, "elza")
-                    })
-                },
-                function(callback) {
-                    var happy = new Hero({ nick: 'happy' })
-                    happy.save(function(err, happy) {
-                        callback(err, "happy")
-                    })
-                },
-            ],
-            function(err, result) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log("Успешно созданы герои с никами: " + result.join(", "))
-                }
-                mongoose.disconnect()
-            })
+    function(err,result){
+        if(err) throw err
         console.log("ok")
     })
-}) 
 
-function open(callback) {
-    mongoose.connection.on("open", callback)
+function open(callback){
+    mongoose.connection.on("open",callback)
 }
 
-function dropDatabase(callback) {
+function dropDatabase(callback){
     var db = mongoose.connection.db
     db.dropDatabase(callback)
 }
 
-function createHeroes(callback) {
-    async.parallel([
-            function(callback) {
-                var natsu = new Hero({ nick: 'natsu' })
-                natsu.save(function(err, natsu) {
-                    callback(err, "natsu")
-                })
-            },
-            function(callback) {
-                var elza = new Hero({ nick: 'elza' })
-                elza.save(function(err, elza) {
-                    callback(err, "elza")
-                })
-            },
-            function(callback) {
-                var happy = new Hero({ nick: 'happy' })
-                happy.save(function(err, happy) {
-                    callback(err, "happy")
-                })
-            },
-        ],
-        function(err, result) {
-            callback(err)
-        })
+function createHeroes(callback){
+    async.each(data, function(heroData, callback){
+            var hero = new mongoose.models.Hero(heroData)
+            hero.save(callback)
+        },
+        callback)
 }
 
-function close(callback) {
+function close(callback){
     mongoose.disconnect(callback)
-} 
+}
