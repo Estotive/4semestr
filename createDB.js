24223ba83a60080f7dca271de68d3fb380e948df
Estotive <martinyuk.d.i.20@gmail.com>
2022-06-27 00:01:55 +0300

    8.4

--- createDB.js
+++ createDB.js
@@ -1,13 +1,39 @@
+var async = require ("async")
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/all')
 var Hero = require("./models/hero").Hero
+mongoose.connection.on("open",function(){
+    var db = mongoose.connection.db
+    db.dropDatabase(function(err){
+        if(err) throw err
 
-var hero = new Hero({
-    title: "Нацу",
-    nick: "natsu"
-})
-
-console.log(hero)
-hero.save(function(err, hero, affected){
-    console.log(hero.title)
+        async.parallel([
+            function(callback){
+                var natsu = new Hero({nick:"natsu"})
+                natsu.save(function(err,natsu){
+                    callback(err,"natsu")
+                })
+            },
+            function(callback){
+                var elza = new Hero({nick:"elza"})
+                elza.save(function(err,elza){
+                    callback(err,"elza")
+                })
+            },
+            function(callback){
+                var happy = new Hero({nick:"happy"})
+                happy.save(function(err,happy){
+                    callback(err,"happy")
+                })
+            }
+        ],
+        function(err,result){
+            if(err){
+                console.log(err)
+            } else {
+                console.log("Успешно созданы герои с никами: " +result.join(", "))
+            }
+            mongoose.disconnect()
+        })
+    })
 })
